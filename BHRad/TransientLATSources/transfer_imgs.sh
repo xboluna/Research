b00955a4e80b3cf580e2dd8ebc9aedba42638d30
xboluna <xboluna@github.com>
2023-02-22 15:01:37 -0800

    Finally got the detection curves!

--- BHRad/TransientLATSources/transfer_imgs.sh
+++ BHRad/TransientLATSources/transfer_imgs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir ./unassociated/
+mkdir ./correct_unassociated/
 
-for i in 4 5 6 13 15 16 19 23 25 26 27 29 31 33 35 36 46 50 58 63 64 76 82 84 95 99 100 104 108 112 116 121 123 126 127 130 132 134 #141 142
+for i in 4 5 6 13 15 16 19 23 25 26 27 29 32 34 44 48 56 61 62 74 80 91 95 96 100 104 108 112 117 119 122 123 126 128 130
 do
-	mv ./apjsac072afigset17/*_"$i"_hr* ./unassociated/
+	mv ./unassociated/*_"$i"_hr* ./correct_unassociated/
 done
